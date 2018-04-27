ca93531eb320f05fad27ba7790e4ac33cccc0b24
pinvondev <pinvondev@gmail.com>
2018-04-27 08:43:26 +0800

    ego

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "http://elpa.emacs-china.org/melpa/") t)
+             ;;'("melpa" . "http://elpa.emacs-china.org/melpa/") t)
+             ;;'("melpa". "http://stable.melpa.org/packages") t)
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -27,9 +29,10 @@
  '(column-number-mode 1)
  '(flymake-google-cpplint-command "/home/pinvon/.local/bin/cpplint")
  '(menu-bar-mode nil)
+ '(org-agenda-files (quote ("~/GTD/gtd.org")))
  '(package-selected-packages
    (quote
-    (google-c-style zygospore projectile flymake-cursor flymake-google-cpplint auto-complete-c-headers auto-complete yasnippet org-page auctex sr-speedbar monokai-theme helm volatile-highlights use-package undo-tree counsel-projectile)))
+    (pug-mode go-mode google-c-style zygospore projectile flymake-cursor flymake-google-cpplint auto-complete-c-headers auto-complete yasnippet auctex sr-speedbar monokai-theme helm volatile-highlights use-package undo-tree counsel-projectile)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 
@@ -39,6 +42,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Noto Sans CJK SC" :foundry "GOOG" :slant normal :weight normal :height 120 :width normal)))))
+
+(custom-set-faces
+ '(org-table ((t (:foreground "#6c71c4" :family "Ubuntu Mono")))))
 
 (defun open-init-file ()
   (interactive)
@@ -56,8 +62,9 @@
 (setq electric-pair-pairs '(
 			    (?\{ . ?\})
 			    (?\' . ?\')
-                	    (?\" . ?\")
-			    ))
+                (?\" . ?\")
+                (?\( . ?\))
+                ))
 
 (electric-pair-mode t)
 
@@ -100,30 +107,6 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
-(require 'org-page)
-(setq op/repository-directory "~/myblog")   ;; the repository location
-(setq op/site-domain "http://pinvondev.github.io/")         ;; your domain
-;;; the configuration below you should choose one, not both
-(setq op/personal-disqus-shortname "pw lin")    ;; your disqus commenting system
-;(setq op/personal-duoshuo-shortname "your_duoshuo_shortname")  ;; your duoshuo commenting system
-(setq op/hashover-comments t)                                   ;; activate hashover self-hosted comment system
-(setq op/personal-github-link "https://github.com/pinvondev")
-(setq op/site-main-title "Pinvon's Blog")
-(setq op/site-sub-title "所见，所闻，所思，所想")
-(setq op/category-config-alist
-      (cons '("essay" ;; 目录名
-              :show-meta t ;; 是否显示摘要
-              :show-comment t ;;
-              :uri-generator op/generate-uri
-              :uri-template "/essay/%y/%m/%d/%t/"
-              :sort-by :date
-              :category-index t)
-            op/category-config-alist))
-
-;;; org-mode字体颜色
-(add-to-list 'org-emphasis-alist
-             '("*" (:foreground "#FFA07A")))
-
 ;;; org-mode自动换行
 (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
 
@@ -153,9 +136,9 @@
 (add-hook 'c-mode-hook 'my:flymake-google-init)
 (add-hook 'c++-mode-hook 'my:flymake-google-init)
 
-(require 'google-c-style)
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+;; (require 'google-c-style)
+;; (add-hook 'c-mode-common-hook 'google-set-c-style)
+;; d-hook 'c-mode-common-hook 'google-make-newline-indent)
 
 (semantic-mode 1)
 (defun my:add-semantic-to-autocomplete()
@@ -164,3 +147,35 @@
 (global-ede-mode 1)
 
 ;; (ede-cpp-root-project "my project" :file "~/demos/my_program/src/main.cpp" :include-path '("/../my_inc") :system-include-path '("/usr/include/c++/3.2.2/"))
+
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 4)
+(setq c-default-style "k&r")
+(setq c-basic-offset 4)
+
+(setq org-log-done 'time)
+
+
+;; the following is only needed if you install EGO manually
+(add-to-list 'load-path "~/.emacs.d/EGO")
+
+(require 'ego)
+
+(ego-add-to-alist 'ego-project-config-alist
+               `(("pinvon" ; 站点工程的名字
+                 :repository-directory "~/myblog" ; 站点的本地目录
+                 :site-domain "https://pinvondev.github.io/" ; 站点的网址
+                 :site-main-title "Pinvon's Blog" ; 站点的标题
+                 :site-sub-title "所见, 所闻, 所思, 所想" ; 站点的副标题
+                 :theme (default) ; 使用的主题
+                 :summary (("years" :year :updates 10) ("authors" :authors) ("tags" :tags)) ; 导航栏的设置，有 category 和 summary 两种
+                 :source-browse-url ("Github" "https://github.com/pinvondev") ; 你的工程源代码所在的位置
+                 :personal-disqus-shortname "pinvon" ; 使用 disqus 评论功能的话，它的短名称
+                 ;; :personal-duoshuo-shortname "emacs-china" ; 使用 多说 评论功能的话，它的短名称(由于多说已经关闭，这里就此废置)
+                 :confound-email nil ; 是否保护邮件名称呢？t 是保护，nil 是不保护，默认是保护
+                 :ignore-file-name-regexp "readme.org" ; 有些不想发布成 html 的 org 文件（但是又想被导入 git 进行管理），可以用这种正则表达的方式排除
+                 :web-server-docroot "~/pinvon-test" ; 本地测试的目录
+                 :web-server-port 5432); 本地测试的端口
+
+                 ;; 你可以在此添加更多的站点设置
+               ))
