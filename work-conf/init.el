b2a8cb30bae4a8cbf6ac72c80c2ebe53c4e13be8
pinvondev <pinvondev@gmail.com>
2019-10-23 15:44:15 +0800

    增加 gtd 相关内容

--- work-conf/init.el
+++ work-conf/init.el
@@ -32,7 +32,7 @@
  '(org-agenda-files (quote ("~/GTD/gtd.org")))
  '(package-selected-packages
    (quote
-    (fill-column-indicator anaconda-mode toc-org org-mind-map pug-mode go-mode google-c-style zygospore projectile flymake-cursor flymake-google-cpplint company auctex sr-speedbar monokai-theme helm volatile-highlights use-package undo-tree counsel-projectile)))
+    (org-pomodoro fill-column-indicator anaconda-mode toc-org org-mind-map pug-mode go-mode google-c-style zygospore projectile flymake-cursor flymake-google-cpplint company auctex sr-speedbar monokai-theme helm volatile-highlights use-package undo-tree counsel-projectile)))
  '(python-shell-interpreter "python")
  '(show-paren-mode t)
  '(tool-bar-mode nil))
@@ -135,3 +135,18 @@
 (setq fci-rule-width 10)
 (setq fci-rule-color "orange")
 (setq fci-rule-column 72)
+
+;; gtd
+;; 捕获临时信息 C-c c; 输入完成后 C-c C-c 保存到 org-directory/notes.org 中
+;; 查看 org-directory 的值: C-h v org-directory
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+(define-key global-map "\C-cc" 'org-capture)
+
+;; 归档
+;; C-c C-x C-a: 保存在当前目录下 xx_archive 文件中
+;; C-c C-x C-s: 光标处所在子树归档
+
+;; 设置 org-agenda 快捷键
+;; org-agenda 会从 org-agenda-files 目录中收集所有的任务并呈现
+(global-set-key "\C-ca" 'org-agenda)
+(setq org-agenda-files '("/root/notes/GTD/"))
